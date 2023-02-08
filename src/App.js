c014bc6dacddcf8f1b6b468ad5a56b1f9ea0ef1e
Puchana <puchana123@gmail.com>
2023-02-08 17:16:28 +0700

    add cart page and finish single product page

--- src/App.js
+++ src/App.js
@@ -19,6 +19,7 @@ import RefundPolicy from './pages/RefundPolicy';
 import ShippingPolicy from './pages/ShippingPolicy';
 import TermAndConditions from './pages/TermAndConditions';
 import SingleProduct from './pages/SingleProduct';
+import Cart from './pages/Cart';
 
 function App() {
   return (
@@ -37,6 +38,7 @@ function App() {
             <Route path='compare' element={<CompareProduct />}/>
             <Route path='wishlist' element={<Wishlist />}/>
             <Route path='login' element={<Login />}/>
+            <Route path='cart' element={<Cart />}/>
             <Route path='forgot-password' element={<ForgotPassword />}/>
             <Route path='signup' element={<SignUp />}/>
             <Route path='reset-password' element={<ResetPassword />}/>
--- src/components/Color.js
+++ src/components/Color.js
@@ -1,7 +1,7 @@
 const Color = ()=>{
     return(
         <>
-            <ul className="colors ps-0">
+            <ul className="colors ps-0 mb-1">
                 <li></li>
                 <li></li>
                 <li></li>
